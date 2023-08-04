5a8cc0d85fe11d3f2e9ef20aeb8ad7e908419856
sahanarudrappa <sahanarudrappa@users.noreply.github.com>
2023-08-04 16:15:10 +1000

    using connection string

--- sqlapp/Pages/Index.cshtml.cs
+++ sqlapp/Pages/Index.cshtml.cs
@@ -7,11 +7,16 @@ namespace sqlapp.Pages
 {
     public class IndexModel : PageModel
     {
-        public List<Product> Products; 
+        public List<Product> Products;
+        private readonly IProductService _productService;
+
+        public IndexModel(IProductService productService)
+        {
+            _productService = productService;
+        }
         public void OnGet()
         {
-            ProductService productService = new ProductService();
-            Products = productService.GetProducts();
+            Products = _productService.GetProducts();
 
         }
     }
--- sqlapp/Services/ProductService.cs
+++ sqlapp/Services/ProductService.cs
@@ -5,33 +5,28 @@ namespace sqlapp.Services
 {
 
     // This service will interact with our Product data in the SQL database
-    public class ProductService
+    public class ProductService : IProductService
     {
-        private static string db_source = "talssqlserver01.database.windows.net";
-        private static string db_user = "sqladmin";
-        private static string db_password = "Preethu143";
-        private static string db_database = "talsappdb";
-
+        private readonly IConfiguration _configuration;
+        public ProductService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
         private SqlConnection GetConnection()
         {
-            
-            var _builder = new SqlConnectionStringBuilder();
-            _builder.DataSource = db_source;
-            _builder.UserID = db_user;
-            _builder.Password = db_password;
-            _builder.InitialCatalog = db_database;
-            return new SqlConnection(_builder.ConnectionString);
+
+            return new SqlConnection(_configuration.GetConnectionString("SQLConnection"));
         }
         public List<Product> GetProducts()
         {
             List<Product> _product_lst = new List<Product>();
             string _statement = "SELECT ProductID,ProductName,Quantity from Products";
             SqlConnection _connection = GetConnection();
-            
+
             _connection.Open();
-            
+
             SqlCommand _sqlcommand = new SqlCommand(_statement, _connection);
-            
+
             using (SqlDataReader _reader = _sqlcommand.ExecuteReader())
             {
                 while (_reader.Read())
